438512f23ca04f576051e05ff0823458fc316732
Aaron R Miller <aaron.miller@waweb.io>
2020-09-22 13:06:19 -0700

    Fixed resource classpath location
    Signed-off-by: Aaron R Miller <aaron.miller@waweb.io>

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -4,20 +4,19 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 
-
 public class MainVerticle extends AbstractVerticle {
 
-  @Override
-  public void start() {
-    final Router router = Router.router(vertx);
+	@Override
+	public void start() {
+		final Router router = Router.router(vertx);
 
-    router.get("/greeting").handler(req -> req.response().end("Hello Vert.x!"));
+		router.get("/greeting").handler(req -> req.response().end("Hello Vert.x!"));
 
-    router.route("/*").handler(StaticHandler.create());
+		router.route().handler(StaticHandler.create());
 
-    vertx.createHttpServer()
-        .requestHandler(router)
-        .listen(8080);
-  }
+		vertx.createHttpServer()
+				.requestHandler(router)
+				.listen(8080);
+	}
 
 }
--- src/test/java/io/vertx/starter/MainVerticleTest.java
+++ src/test/java/io/vertx/starter/MainVerticleTest.java
@@ -1,62 +1,52 @@
 package io.vertx.starter;
 
-import io.vertx.core.Vertx;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.codec.BodyCodec;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 
 @ExtendWith(VertxExtension.class)
 class MainVerticleTest {
 
-  @BeforeEach
-  void prepare(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(MainVerticle.class.getCanonicalName(), testContext.succeeding(id -> testContext.completeNow()));
-  }
+	@BeforeEach
+	void prepare(final Vertx vertx, final VertxTestContext testContext) {
+		vertx.deployVerticle(MainVerticle.class.getCanonicalName(),
+				testContext.succeeding(id -> testContext.completeNow()));
+	}
 
-  @Test
-  @DisplayName("Check that the server returns greeting")
-  void checkServerHasStarted(Vertx vertx, VertxTestContext testContext) {
-    WebClient webClient = WebClient.create(vertx);
-    webClient.get(8080, "localhost", "/greeting")
-      .as(BodyCodec.string())
-      .send(testContext.succeeding(response -> testContext.verify(() -> {
-        assertEquals(200, response.statusCode());
-        assertTrue(response.body().length() > 0);
-        assertTrue(response.body().contains("Hello Vert.x!"));
-        testContext.completeNow();
-      })));
-  }
+	@Test
+	@DisplayName("Check that the server returns greeting")
+	void checkServerHasStarted(final Vertx vertx, final VertxTestContext testContext) {
+		final WebClient webClient = WebClient.create(vertx);
+		webClient.get(8080, "localhost", "/greeting")
+				.as(BodyCodec.string())
+				.send(testContext.succeeding(response -> testContext.verify(() -> {
+					assertEquals(200, response.statusCode());
+					assertTrue(response.body().length() > 0);
+					assertTrue(response.body().contains("Hello Vert.x!"));
+					testContext.completeNow();
+				})));
+	}
 
-  @Test
-  @DisplayName("Check that the server returns router-root.txt")
-  void checkServerHasRootResource(Vertx vertx, VertxTestContext testContext) {
-    WebClient webClient = WebClient.create(vertx);
-    webClient.get(8080, "localhost", "/router-root.txt")
-      .as(BodyCodec.string())
-      .send(testContext.succeeding(response -> testContext.verify(() -> {
-        assertEquals(200, response.statusCode());
-        testContext.completeNow();
-      })));
-  }
+	@Test
+	@DisplayName("Check that the server returns hello-webroot.txt")
+	void checkServerHasRootResource(final Vertx vertx, final VertxTestContext testContext) {
+		final WebClient webClient = WebClient.create(vertx);
+		webClient.get(8080, "localhost", "/hello-webroot.txt")
+				.as(BodyCodec.string())
+				.send(testContext.succeeding(response -> testContext.verify(() -> {
+					assertEquals(200, response.statusCode());
+					testContext.completeNow();
+				})));
+	}
 
-    
-  @Test
-  @DisplayName("Check that the server returns router-package.txt")
-  void checkServerHasPackageResource(Vertx vertx, VertxTestContext testContext) {
-    WebClient webClient = WebClient.create(vertx);
-    webClient.get(8080, "localhost", "/router-package.txt")
-      .as(BodyCodec.string())
-      .send(testContext.succeeding(response -> testContext.verify(() -> {
-        assertEquals(200, response.statusCode());
-        testContext.completeNow();
-      })));
-  }
 }
